7893b74c470d3a6be76caa6aae984ac46e2657ce
Your Name <you@example.com>
2023-03-18 14:51:04 +0100

    Document crate itself

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 #![no_std]
+//! This crate provides a type ([`Finalizable`]) for values that can be finalized,
+//! with methods that operate on working values but leave finalized values unchanged.
 
 pub use Finalizable::*;
 
